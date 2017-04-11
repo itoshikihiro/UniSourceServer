2da5b93d02550f9d4ffd0acd45f9d4f9d6d9875d
itoshikihiro <1127504477@qq.com>
2017-04-11 17:08:42 -0400

    update

--- src/com/un/service/impl/LoginSvcImpl.java
+++ src/com/un/service/impl/LoginSvcImpl.java
@@ -8,15 +8,43 @@
     */  
 package com.un.service.impl;
 
+import com.un.service.LoginSvc;
 
-    /** 
+/** 
     * @ClassName: LoginSvcImpl 
     * @Description: the implements of login service
     * @author Jie Lin: kimihiro.lin80@gmail.com
     * @date Apr 11, 2017 1:19:39 PM 
     *  
     */
-public class LoginSvcImpl {
+public class LoginSvcImpl implements LoginSvc{
+
+	/* (non Javadoc) 
+	* <p>Title: checkUser</p> 
+	* <p>Description: </p> 
+	* @param userID
+	* @return 
+	* @see com.un.service.LoginSvc#checkUser(java.lang.String) 
+	*/ 
+	@Override
+	public boolean checkUser(String userID) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non Javadoc) 
+	* <p>Title: validUser</p> 
+	* <p>Description: </p> 
+	* @param userID
+	* @param password
+	* @return 
+	* @see com.un.service.LoginSvc#validUser(java.lang.String, java.lang.String) 
+	*/ 
+	@Override
+	public boolean validUser(String userID, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
 
---
+++ src/com/un/tool/UserListReader.java
@@ -0,0 +1,35 @@
+    /**   
+    * @Title: UserListReader.java 
+    * @Package com.un.tool 
+    * @Description: TODO 
+    * @author Jie Lin: kimihiro.lin80@gmail.com 
+    * @date Apr 11, 2017 5:02:35 PM 
+    * @version V1.0   
+    */  
+package com.un.tool;
+
+import java.util.ArrayList;
+
+import com.un.pojo.Student;
+import com.un.pojo.User;
+
+/** 
+    * @ClassName: UserListReader 
+    * @Description: TODO
+    * @author Jie Lin: kimihiro.lin80@gmail.com
+    * @date Apr 11, 2017 5:02:35 PM 
+    *  
+    */
+public class UserListReader {
+	public static ArrayList<Student> getStudentList(){
+		ArrayList<Student> rev = new ArrayList<Student>();
+		
+		return null;
+	}
+	
+	public static ArrayList<User> getAdminList(){
+		
+		return null;
+	}
+}
+
