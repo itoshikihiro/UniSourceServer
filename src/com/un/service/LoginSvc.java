84b0f0e74520abb1bbb50eec8e76a82f34f9a18f
itoshikihiro <1127504477@qq.com>
2017-04-11 13:24:35 -0400

    update login service and service impl

---
+++ src/com/un/service/LoginSvc.java
@@ -0,0 +1,22 @@
+    /**   
+    * @Title: LoginSvc.java 
+    * @Package com.un.service 
+    * @Description: TODO 
+    * @author Jie Lin: kimihiro.lin80@gmail.com 
+    * @date Apr 11, 2017 1:19:08 PM 
+    * @version V1.0   
+    */  
+package com.un.service;
+
+
+    /** 
+    * @ClassName: LoginSvc 
+    * @Description: the interface for login service
+    * @author Jie Lin: kimihiro.lin80@gmail.com
+    * @date Apr 11, 2017 1:19:08 PM 
+    *  
+    */
+public interface LoginSvc {
+
+}
+
---
+++ src/com/un/service/impl/LoginSvcImpl.java
@@ -0,0 +1,22 @@
+    /**   
+    * @Title: LoginSvcImpl.java 
+    * @Package com.un.service.impl 
+    * @Description: TODO 
+    * @author Jie Lin: kimihiro.lin80@gmail.com 
+    * @date Apr 11, 2017 1:19:39 PM 
+    * @version V1.0   
+    */  
+package com.un.service.impl;
+
+
+    /** 
+    * @ClassName: LoginSvcImpl 
+    * @Description: the implements of login service
+    * @author Jie Lin: kimihiro.lin80@gmail.com
+    * @date Apr 11, 2017 1:19:39 PM 
+    *  
+    */
+public class LoginSvcImpl {
+
+}
+
