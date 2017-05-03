8d0130e39f44f89cd161ede25ffc11be161cbbc7
itoshikihiro <1127504477@qq.com>
2017-05-03 03:40:21 -0400

    test client for download pdf

--- src/com/un/main/Client.java
+++ src/com/un/main/Client.java
@@ -9,6 +9,9 @@
 package com.un.main;
 
 import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -34,72 +37,72 @@ public class Client {
 			Socket socket = new Socket("127.0.0.1", 6666);
 			//time out for 60s
 			//socket.setSoTimeout(60000);
-			
+
 			//send object to client
 			ObjectOutputStream objectWriter= new ObjectOutputStream(socket.getOutputStream());
 			//get object from client
 			ObjectInputStream objectReader = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
-			
+
 			System.out.println("Get the connection");
-			
+
 			//test the correct match
 			User u = new User();
 			u.setPassword("a1234567890");
 			u.setUserID("jabigelow@ursinus.edu");
 			u.setRoleID(1);
 			u.setUsername(null);
-			
+
 			objectWriter.writeObject(new Message(0,u));
 			objectWriter.flush();//send object to server
-			
+
 			System.out.println("object has sent to server");
-			
+
 			Object obj = objectReader.readObject();
 			if(obj!=null){
- 				Message m = (Message) obj;
- 				if(m.getTaskCode()==-1){
- 					System.out.println("something goes wrong");
- 				}else{
- 					Student s = (Student) m.getObject();
- 					System.out.println(s.toString());
- 				}
+				Message m = (Message) obj;
+				if(m.getTaskCode()==-1){
+					System.out.println("something goes wrong");
+				}else{
+					Student s = (Student) m.getObject();
+					System.out.println(s.toString());
+				}
 			}
-			
+
 			//test for null user
 			objectWriter.writeObject(new Message(0,new User()));
 			objectWriter.flush();//send object to server
-			
+
 			System.out.println("object has sent to server");
-			
+
 			obj = objectReader.readObject();
 			if(obj!=null){
- 				Message m = (Message) obj;
- 				if(m.getTaskCode()==-1){
- 					System.out.println("something goes wrong");
- 				}else{
- 					Student s = (Student) m.getObject();
- 					System.out.println(s.toString());
- 				}
+				Message m = (Message) obj;
+				if(m.getTaskCode()==-1){
+					System.out.println("something goes wrong");
+				}else{
+					Student s = (Student) m.getObject();
+					System.out.println(s.toString());
+				}
 			}
-			
-			
+
+
 			//test for unmatched userId and password
 			objectWriter.writeObject(new Message(0,new User("jabigelow@ursinus.edu",null,"1561654564654",1)));
 			objectWriter.flush();//send object to server
-			
+
 			System.out.println("object has sent to server");
-			
+
 			obj = objectReader.readObject();
 			if(obj!=null){
- 				Message m = (Message) obj;
- 				if(m.getTaskCode()==-1){
- 					System.out.println("something goes wrong");
- 				}else{
- 					Student s = (Student) m.getObject();
- 					System.out.println(s.toString());
- 				}
+				Message m = (Message) obj;
+				if(m.getTaskCode()==-1){
+					System.out.println("something goes wrong");
+				}else{
+					Student s = (Student) m.getObject();
+					System.out.println(s.toString());
+				}
 			}
-			
+
 			/** 
 			 * 
 			 * 
@@ -111,7 +114,7 @@ public class Client {
 			/*objectWriter.writeObject(new Message(1, "jilin@ursinus.edu"));
 			objectWriter.flush();
 			System.out.println("object has sent to server to test register process");
-			
+
 			obj = objectReader.readObject();
 			if(obj!=null){
  				Message m = (Message) obj;
@@ -122,7 +125,7 @@ public class Client {
  					System.out.println(str);
  				}
 			}*/
-			
+
 			//When the user logs in, the client end should keep the userId and roleId while the user continue doing some actions
 			/** 
 			 * 
@@ -134,39 +137,39 @@ public class Client {
 			objectWriter.writeObject(new Message(2, new Student("jabigelow@ursinus.edu", null, null, 0, 0, 0, null)));
 			objectWriter.flush();
 			System.out.println("object has sent to server to test get course list process");
-			
+
 			obj = objectReader.readObject();
 			if(obj!=null){
- 				Message m = (Message) obj;
- 				if(m.getTaskCode()==-1){
- 					System.out.println("something goes wrong");
- 				}else{
- 					ArrayList<Course> ac = (ArrayList<Course>) m.getObject();
- 					Iterator<Course> i = ac.iterator();
- 					while(i.hasNext()){
- 						System.out.println(i.next());
- 					}
- 				}
+				Message m = (Message) obj;
+				if(m.getTaskCode()==-1){
+					System.out.println("something goes wrong");
+				}else{
+					ArrayList<Course> ac = (ArrayList<Course>) m.getObject();
+					Iterator<Course> i = ac.iterator();
+					while(i.hasNext()){
+						System.out.println(i.next());
+					}
+				}
 			}
-			
-			
+
+
 			/** 
 			 * 
 			 * 
 			 * 
 			 * 
 			 */
-			//since the student client should not have the permission to delete a course from his course list. The only thing he can do is adding a new course to the list.
+			/*//since the student client should not have the permission to delete a course from his course list. The only thing he can do is adding a new course to the list.
 			objectWriter.writeObject(new Message(31, new Student("jabigelow@ursinus.edu", null, null, 0, 0, 0, null)));
 			objectWriter.flush();
-			
+
 			System.out.println("let server to record user's id");
-			
+
 			objectWriter.writeObject(new Message(32, new Course("CS-170","Basic Introduction of programming")));
 			objectWriter.flush();
-			
+
 			System.out.println("sending the update courses");
-			
+
 			obj = objectReader.readObject();
 			if(obj!=null){
  				Message m = (Message) obj;
@@ -179,8 +182,8 @@ public class Client {
  						System.out.println(i.next());
  					}
  				}
-			}
-			
+			}*/
+
 			/** 
 			 * 
 			 * 
@@ -192,21 +195,21 @@ public class Client {
 			objectWriter.writeObject(new Message(4, new Student("jabigelow@ursinus.edu", null, null, 0, 0, 0, null)));
 			objectWriter.flush();
 			System.out.println("object has sent to server to test get activity list process");
-			
+
 			obj = objectReader.readObject();
 			if(obj!=null){
- 				Message m = (Message) obj;
- 				if(m.getTaskCode()==-1){
- 					System.out.println("something goes wrong");
- 				}else{
- 					ArrayList<Activity> ac = (ArrayList<Activity>) m.getObject();
- 					Iterator<Activity> i = ac.iterator();
- 					while(i.hasNext()){
- 						System.out.println(i.next());
- 					}
- 				}
+				Message m = (Message) obj;
+				if(m.getTaskCode()==-1){
+					System.out.println("something goes wrong");
+				}else{
+					ArrayList<Activity> ac = (ArrayList<Activity>) m.getObject();
+					Iterator<Activity> i = ac.iterator();
+					while(i.hasNext()){
+						System.out.println(i.next());
+					}
+				}
 			}
-			
+
 			/** 
 			 * 
 			 * 
@@ -218,12 +221,12 @@ public class Client {
 			objectWriter.writeObject(new Message(50, new Student("jabigelow@ursinus.edu", null, null, 0, 0, 0, null)));
 			objectWriter.flush();
 			System.out.println("object has sent to server to let server to record the user id");
-			
+
 			//add a new activity
 			objectWriter.writeObject(new Message(51, new Activity("Intership2","A winter intership")));
 			objectWriter.flush();
 			System.out.println("object has sent to server to let server to test if it can add a new activity");
-			
+
 			obj = objectReader.readObject();
 			if(obj!=null){
  				Message m = (Message) obj;
@@ -237,7 +240,7 @@ public class Client {
  					}
  				}
 			}*/
-			
+
 			/** 
 			 * 
 			 * 
@@ -249,12 +252,12 @@ public class Client {
 			objectWriter.writeObject(new Message(50, new Student("jabigelow@ursinus.edu", null, null, 0, 0, 0, null)));
 			objectWriter.flush();
 			System.out.println("object has sent to server to let server to record the user id");
-			
+
 			//delete an activity
 			objectWriter.writeObject(new Message(52, new Activity("Intership2","A winter intership")));
 			objectWriter.flush();
 			System.out.println("object has sent to server to let server to test if it can delete an existed activity");
-			
+
 			obj = objectReader.readObject();
 			if(obj!=null){
  				Message m = (Message) obj;
@@ -268,19 +271,19 @@ public class Client {
  					}
  				}
 			}*/
-			
-			
+
+
 			/*//test for user updates an activity
 			//to test if user can update an activity
 			objectWriter.writeObject(new Message(50, new Student("jabigelow@ursinus.edu", null, null, 0, 0, 0, null)));
 			objectWriter.flush();
 			System.out.println("object has sent to server to let server to record the user id");
-			
+
 			//update an activity
 			objectWriter.writeObject(new Message(53, new Activity("Intership2","A winter intership (change)")));
 			objectWriter.flush();
 			System.out.println("object has sent to server to let server to test if it can update an existed activity");
-			
+
 			obj = objectReader.readObject();
 			if(obj!=null){
  				Message m = (Message) obj;
@@ -294,29 +297,69 @@ public class Client {
  					}
  				}
 			}*/
-			
-			
+
+
 			//update an profile
 			objectWriter.writeObject(new Message(12, new Student("dakritz@ursinus.edu","Danielle", "a9876543210", 1, 2017, 21, "A senior student")));
 			objectWriter.flush();
 			System.out.println("object has sent to server to let server to test if it can update an existed profile");
-			
+
 			obj = objectReader.readObject();
 			if(obj!=null){
- 				Message m = (Message) obj;
- 				if(m.getTaskCode()==-1){
- 					System.out.println("something goes wrong");
- 				}else{
- 					Student s =(Student) m.getObject();
- 					System.out.println(s);
- 				}
+				Message m = (Message) obj;
+				if(m.getTaskCode()==-1){
+					System.out.println("something goes wrong");
+				}else{
+					Student s =(Student) m.getObject();
+					System.out.println(s);
+				}
 			}
-			
-			
+
+			//update an profile
+			objectWriter.writeObject(new Message(81, new Student("jabigelow@ursinus.edu", null, null, 0, 0, 0, null)));
+			objectWriter.flush();
+			System.out.println("object has sent to server to let server to test if it can generate pdf resume");
+
+			objectWriter.writeObject(new Message(82, new Student("jabigelow@ursinus.edu", null, null, 0, 0, 0, null)));
+			objectWriter.flush();
+			System.out.println("object has sent to server to let server to test if it can generate pdf resume");
+
+			DataInputStream dis = null;
+			FileOutputStream fos = null;
+			try {
+				dis = new DataInputStream(socket.getInputStream());
+				//文件名和长度
+				String fileName = dis.readUTF();
+				long fileLength = dis.readLong();
+				fos = new FileOutputStream(new File("d:/" + fileName));
+
+				byte[] sendBytes = new byte[1024];
+				int transLen = 0;
+				System.out.println("----Start to receive file<" + fileName + ">,size is<" + fileLength + ">----");
+				while(true){
+					int read = 0;
+					read = dis.read(sendBytes);
+					if(read == -1)
+						break;
+					transLen += read;
+					System.out.println("receive percentage:" + 100 * transLen/fileLength + "%...");
+					fos.write(sendBytes, 0, read);
+					fos.flush();
+				}
+				System.out.println("----<" + fileName + "> receives successfully-------");
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if(dis != null)
+					dis.close();
+				if(fos != null)
+					fos.close();
+			}
+
 			//shutdown the connection from the client
 			objectWriter.writeObject(new Message(-2,null));
 			objectWriter.flush();//send object to server
-			
+
 			//shutdown the connection
 			objectReader.close();
 			objectWriter.close();
